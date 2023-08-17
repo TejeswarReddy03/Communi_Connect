bdee4700dcb9b9aec57a08bc414d58d36b9848b9
manoj <manojkumargompa@gmail.com>
2023-08-18 00:23:13 +0530

    creatednavbar removed initial signout button

--- app/src/main/java/com/example/communiconnect/MainActivity.java
+++ app/src/main/java/com/example/communiconnect/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.communiconnect;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -15,22 +22,86 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private Button logout;
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if(drawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout = findViewById(R.id.logout);
 
-        logout.setOnClickListener(new View.OnClickListener() {
+//        logout = findViewById(R.id.logout);
+//
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseAuth.getInstance().signOut();
+//                Toast.makeText(MainActivity.this, "Logged Out!", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(MainActivity.this, logreg.class));
+//            }
+//        });
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+
+
+        navigationView=findViewById(R.id.nav_view);
+
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(MainActivity.this, "Logged Out!", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, logreg.class));
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.home) {
+                    Toast.makeText(MainActivity.this, "Home Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.map) {
+                    Toast.makeText(MainActivity.this, "Contact Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.alert) {
+                    Toast.makeText(MainActivity.this, "Gallery Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.groups) {
+                    Toast.makeText(MainActivity.this, "About Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.logoutof) {
+                    Toast.makeText(MainActivity.this, "Login Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.groups) {
+                    Toast.makeText(MainActivity.this, "Share Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.posts) {
+                    Toast.makeText(MainActivity.this, "Rate Us Selected", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
 
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
+
+
 }
